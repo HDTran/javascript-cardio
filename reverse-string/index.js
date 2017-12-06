86aec340080e629668b82ce2e39d526a42a7e759
Huy Tran <huy@beau-coup.com>
2017-12-05 18:56:13 -0800

    feat: Added String Reversal alternative solutions

--- reverse-string/index.js
+++ reverse-string/index.js
@@ -1,5 +1,22 @@
-function reverseString(str) {  
+function reverseString(str) {
   return str.split('').reverse().join(''); // split and join with no delimiter
+}
+
+// alternate solution -- reverse by loop, using for of
+function reverseStringByLoop(str) {
+  let reversed = '';
+
+  for(let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+// alternate solution -- reduce helper
+function reverseByHelper(str) {
+  // reduce takes in function and starting value
+  return str.split('').reduce((reversedChain, characterCurrent) => characterCurrent + reversedChain, '');
 }
 
 module.exports = reverseString;
