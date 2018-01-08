eb5702b33d2fe2ad1ac1732e79f7c8c150b6e9ff
Huy Tran <huy@beau-coup.com>
2018-01-07 16:56:23 -0800

    feat: Vowel counter implemented

---
+++ vowels/index.js
@@ -0,0 +1,20 @@
+// A function that takes a string and returns number of vowels (aeiou)
+function vowels(str) {
+  let counter = 0;
+  const vowelChecker = ['a', 'e', 'i', 'o', 'u'];
+
+  for(let character of str.toLowerCase()) {
+    if(vowelChecker.includes(character)) {
+      counter++;
+    }
+  }
+
+  return counter;
+}
+
+function vowelsByRegex(str) {
+  let matchArr = str.toLowerCase().match(/[aeiou]/g);
+  return matchArr === null ? 0 : matchArr.length;
+}
+
+module.exports = vowels;
