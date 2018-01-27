ca1a325d3d4c8457c7f4d205d6dadb9083aadc0a
Huy Tran <huy@beau-coup.com>
2018-01-26 18:11:56 -0800

    feat: Basic LinkedList skeleton work

---
+++ linkedlist/index.js
@@ -0,0 +1,16 @@
+// Implement classes Node and Linked Lists
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
+
+module.exports = { Node, LinkedList };
